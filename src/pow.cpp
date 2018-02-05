7afcb3fe584df57f0689fdbf63a5b196467c0a6a
Kiss Gergely <itisgergo@gmail.com>
2018-02-05 18:15:47 +0100

    pow átnevezése powerre és pow paraméter->exponential closes #1

--- src/pow.cpp
+++ src/pow.cpp
@@ -5,14 +5,14 @@ using namespace std;
 //STARTCOPY
 namespace mytl{
 template<typename T, typename P>
-T pow(T base, P pow, T unit){
+T power(T base, P exponential, T unit){
     T res = unit;
-    while(pow > 0){
-        if(pow%2 == P(1)) res = res * base;
+    while(exponential > 0){
+        if(exponential%2 == P(1)) res = res * base;
         base = base * base;
-        pow = pow / 2;
+        exponential = exponential / 2;
     }
     return res;
 }
-};
+}
 //ENDCOPY
