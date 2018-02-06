27c817e6232ba400089bec52b96476b4c7450d75
Kiss Gergely <itisgergo@gmail.com>
2018-02-06 19:11:47 +0100

    #7 Point basic functionality

--- src/geom.cpp
+++ src/geom.cpp
@@ -1,9 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+using ll = long long;
 
 //STARTCOPY
 namespace mytl{
+
+struct Point{
+    ll x, y;
+
+    Point(ll x, ll y) : x{x}, y{y} {};
+    Point(pair<ll,ll> initPair) : x{initPair.first}, y{initPair.second} {};
+
+    pair<ll,ll> getPair(){//not introducing type cast operator for safety
+        return {x,y};
+    }
+
+    //Unary operators
+    Point operator-() const{
+        return {-x, -y};
+    }
+    Point operator+() const{
+        return {x,y};
+    }
+
+    static ll sgn(ll x){
+        return (x > 0) - (x < 0);
+    }
+
+    //Binary operators on ordinary numbers
+    Point operator*(const ll& operand) const{
+        return {x*operand, y*operand};
+    }
+
+    //Binary operators on Point itself
+    Point operator+(const Point& operand) const{
+        return {x+operand.x, y+operand.y};
+    }
+    Point operator-(const Point& operand) const{
+        return {x-operand.x, y-operand.y};
+    }
+
+    ll operator*(const Point& operand){ //vectorial product
+        return x*operand.y - y*operand.x;
+    }
+
+    ll direction(const Point& a, const Point& b) const{
+        return sgn((a - *this) * (b - *this));
+    };
+};
 
 }
 //ENDCOPY
--- test/geom_test.cpp
+++ test/geom_test.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #include "../cbp/test.h"
+#include "../src/geom.cpp"
 using namespace std;
 
 
@@ -7,5 +8,7 @@ using namespace std;
 
 
 void geom_test(){
-
+    assert((mytl::Point{0,0}.direction({1,1}, {2,1}) == -1
+         && mytl::Point{0,0}.direction({2,2}, {1,1}) == 0
+         && mytl::Point{0,0}.direction({-1,1}, {1, -1}) == 0));
 }
