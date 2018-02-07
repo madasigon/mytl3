983788b84000d4957d8ac98c1c0ea9fed33d6454
Kiss Gergely <itisgergo@gmail.com>
2018-02-07 15:00:04 +0100

    Unionfind basic func..

--- src/unionfind.cpp
+++ src/unionfind.cpp
@@ -6,7 +6,30 @@ using ll = long long;
 #define UNIONFIND_CPP
 
 namespace mytl{
+    struct UnionFind{
+        vector<int> t, mysize;
+        UnionFind(int n) : t(n+1, -2), mysize{n+1, 1} {};
 
+        int anc(int x){
+            if(t[x] == -2) return x;
+            else{
+                t[x] = anc(t[x]);
+                return t[x];
+            }
+        }
+        bool together(int a, int b){
+            return anc(a) == anc(b);
+        }
+        void unite(int a, int b){
+            int anca = anc(a), ancb = anc(b);
+            if(anca != ancb){
+                if(mysize[anca] > mysize[ancb]){
+                    t[ancb] = anca;
+                    mysize[anca] += mysize[ancb];
+                }
+            }
+        }
+    };
 }
 
 #endif
--- test/unionfind_test.cpp
+++ test/unionfind_test.cpp
@@ -4,7 +4,13 @@
 using namespace std;
 using namespace mytl;
 
+UnionFind un(10);
 
 void unionfind_test(){
-
+    /*while(1){
+        int a,b;
+        cin>>a>>b;
+        cout<<un.together(a, b)<<endl;
+        un.unite(a,b);
+    }*/
 }
