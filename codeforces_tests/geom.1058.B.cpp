bd6e765901a5630c25f0260732635312b3adb7db
Kiss Gergely <gergely.kiss@lynxanalytics.com>
2018-09-26 19:23:18 +0200

    new test for geometry (weak)

---
+++ codeforces_tests/geom.1058.B.cpp
@@ -0,0 +1,652 @@
+//STARTCOPY
+#include<bits/stdc++.h>
+using namespace std;
+
+using u_counter_type = unsigned int;
+using counter_type = int;
+using MAIN = int;
+#define int DontUseIntÉ
+using ll = long long;
+
+template<typename T>
+using PairOf = pair<T,T>;
+namespace mytl{
+
+template<typename T>
+struct Maybe{
+protected:
+    T value;
+    bool is_just = false;
+
+public:
+    Maybe(){}
+    Maybe(T val) : value{value}, is_just{true} {};
+
+    static Maybe<T> just(T val){
+        return Maybe<T>(val);
+    }
+
+    void operator=(T val){
+        is_just = true;
+        value = val;
+    }
+
+    T get(){
+        assert(is_just);
+        return *value;
+    }
+    T get(T def){
+        if(is_just){
+            return *value;
+        }
+        else{
+            return def;
+        }
+    }
+};
+
+template<typename T>
+struct Tracker : Maybe<T>{
+    using Maybe<T>::is_just;
+    using Maybe<T>::get;
+    using Maybe<T>::Maybe;
+    function<T(T,T)> f;
+
+    Tracker(function<T(T,T)> f){
+        Maybe<T>();
+        f = f;
+    }
+
+    T update(T new_val){
+        if(is_just){
+            *this = f(get(), new_val);
+        }
+        else{
+            *this = new_val;
+        }
+    }
+};
+
+
+
+
+
+
+
+}
+namespace mytl{
+template<typename T, typename P>
+T power(T base, P exponential, T unit){
+    T res = unit;
+    while(exponential > 0){
+        if(exponential%2 == P(1)) res = res * base;
+        base = base * base;
+        exponential = exponential / 2;
+    }
+    return res;
+}
+}
+namespace mytl{
+    template<ll MOD>
+    struct TSModulo{
+    private:
+        ll val;
+
+    public:
+        static TSModulo unit(){
+            return 1;
+        }
+        static TSModulo inverse(TSModulo x){// asserting MOD is prime
+            return power(x, MOD-2, unit());
+        };
+        TSModulo(ll initVal){
+            initVal %= MOD;
+            if(val < 0) initVal += MOD;
+            val = initVal;
+        };
+        TSModulo() : TSModulo(0) {}
+        TSModulo& operator=(const TSModulo&) = default;
+
+        ll get() const{ // no type cast operator to prevent accidentally turning into ordinary number
+            return val;
+        }
+
+        //Unary operators
+        TSModulo operator-() const{
+            return TSModulo(-val);
+        };
+        TSModulo operator+() const{
+            return TSModulo(+val);
+        };
+
+        //Binary operators on ordinary numbers
+        TSModulo operator-(const ll& operand) const{
+            return TSModulo(val-operand);
+        };
+        TSModulo operator+(const ll& operand) const{
+            return TSModulo(val+operand);
+        };
+        TSModulo operator*(const ll& operand) const{
+            return TSModulo(val*operand);
+        };
+        TSModulo operator/(const ll& operand) const{ //asserting MOD is prime
+            return TSModulo(inverse(operand) * val);
+        };
+
+        //Binary operators on Modulo
+        TSModulo operator-(const TSModulo& operand) const{
+            return TSModulo(val-operand.get());
+        };
+        TSModulo operator+(const TSModulo& operand) const{
+            return TSModulo(val+operand.get());
+        };
+        TSModulo operator*(const TSModulo& operand) const{
+            return TSModulo(val*operand.get());
+        };
+        TSModulo operator/(const TSModulo& operand) const{ //asserting MOD is prime
+            return TSModulo(val * inverse(operand));
+        };
+    };
+
+    using Mod107 = TSModulo<1000000007LL>;
+
+    template<ll MOD>
+    ostream& operator<<(ostream& os, TSModulo<MOD> x){
+        return os<<"("<<x.get()<<"%"<<MOD<<")";
+    }
+
+
+}
+namespace mytl{
+    template <typename R, typename... Args>
+    function<R (Args...)> memo(R (*fn)(Args...)) {
+        map<tuple<Args...>, R> table;
+        return [fn, table](Args... args) mutable -> R {
+            auto argt = make_tuple(args...);
+            auto memoized = table.find(argt);
+            if(memoized == table.end()) {
+                auto result = fn(args...);
+                table[argt] = result;
+                return result;
+            } else {
+                return memoized->second;
+            }
+        };
+    }
+
+    template <typename R, typename Arg>
+    function<R (Arg)> unordered_memo(R (*fn)(Arg)) {
+        unordered_map<Arg, R> table;
+        return [fn, table](Arg arg) mutable -> R {
+            auto memoized = table.find(arg);
+            if(memoized == table.end()) {
+                auto result = fn(arg);
+                table[arg] = result;
+                return result;
+            } else {
+                return memoized->second;
+            }
+        };
+    }
+
+    template <typename R, typename Arg>
+    function<R (Arg)> natural_memo(R (*fn)(Arg)) {
+        vector<pair<bool, R> > table;
+        return [fn, table](Arg arg) mutable -> R {
+            if(table.size() <= arg) table.resize(arg+1, {false, R()});
+            if(!table[arg].first) {
+                table[arg] = {true, fn(arg)};
+            }
+            return table[arg].second;
+        };
+    }
+}
+namespace mytl{
+
+}
+namespace mytl{
+    struct Modulo{
+    private:
+        ll val;
+    public:
+        const ll MOD;
+        Modulo(ll initMOD, ll initVal) : MOD{initMOD}, val{initVal} {
+            val %= MOD;
+            if(val < 0) val += MOD;
+        };
+
+        static Modulo inverse(Modulo x){// asserting MOD is prime
+            return power(x, x.MOD-2, x.unit());
+        };
+        Modulo unit() const{
+            return Modulo(MOD, 1);
+        }
+        Modulo& operator=(const Modulo& operand){
+            val = operand.val;
+            return *this;
+        };
+
+        ll get() const{ // no type cast operator to prevent accidentally turning into ordinary number
+            return val;
+        }
+
+        //Unary operators
+        Modulo operator-() const{
+            return Modulo(MOD, -val);
+        };
+        Modulo operator+() const{
+            return Modulo(MOD, +val);
+        };
+
+        //Binary operators on ordinary numbers
+        Modulo operator-(const ll& operand) const{
+            return Modulo(MOD, val-operand);
+        };
+        Modulo operator+(const ll& operand) const{
+            return Modulo(MOD, val+operand);
+        };
+        Modulo operator*(const ll& operand) const{
+            return Modulo(MOD, val*operand);
+        };
+        Modulo operator/(const ll& operand) const{ //asserting MOD is prime
+            return Modulo(MOD, val * inverse(Modulo(MOD, operand)).get());
+        };
+
+        //Binary operators on Modulo
+        Modulo operator-(const Modulo& operand) const{
+            return Modulo(MOD, val-operand.get());
+        };
+        Modulo operator+(const Modulo& operand) const{
+            return Modulo(MOD, val+operand.get());
+        };
+        Modulo operator*(const Modulo& operand) const{
+            return Modulo(MOD, val*operand.get());
+        };
+        Modulo operator/(const Modulo& operand) const{ //asserting MOD is prime
+            return Modulo(MOD, val * inverse(operand).get());
+        };
+
+    };
+
+    Modulo mod107(ll x){
+        return Modulo(1000000007LL, x);
+    }
+    ostream& operator<<(ostream& os, Modulo x){
+        return os<<"("<<x.get()<<"%"<<x.MOD<<")";
+    }
+
+
+}
+namespace mytl {
+
+template<class T>
+struct Resetter;
+
+template<class T>
+Resetter<T>* global_resetter = new Resetter<T>;
+
+template<class T>
+struct Resetter {
+
+    using State = pair<counter_type, T>;
+
+    State* state = new State{0, T()};
+
+    Resetter(){}
+    Resetter(T value){
+        state = new State{0, value};
+    }
+
+    void activate(){
+        global_resetter<T> = this;
+    }
+
+    void reset(T value){
+        *state = State{state->first+1, value};
+    }
+
+    struct Variable{
+
+        State state;
+        State& parent = *global_resetter<T>->state;
+
+        Variable() : state{*global_resetter<T>->state} {}
+        Variable(T value) : state{global_resetter<T>->state->first, value} {}
+        Variable(State st) : state{st} {}
+        Variable(T value, State& parent) : state{parent->first, value} {};
+
+        bool fresh(){
+            return state.first >= parent.first;
+        }
+
+        void overwrite(){
+            state = parent;
+        }
+
+        bool refresh(){
+            if(!fresh()) overwrite();
+        }
+
+        T get(){
+            refresh();
+            return state.second;
+        }
+
+        void survive(){
+            state.first = parent.first;
+        }
+
+        void operator=(T new_value){
+            state.second = new_value;
+            survive();
+        }
+
+    };
+
+};
+}
+namespace mytl{
+template<class Q>
+    struct Offline{
+        using Query = typename Q::Query;
+        using T = typename Q::T;
+        typedef pair<Query, function<void(T)> > Tie;
+        vector<Tie> queries;
+        Offline(){};
+
+        void query(Query question, function<void(T)> callback){
+            queries.push_back({question, callback});
+        }
+
+        void process(){
+            sort(begin(queries), end(queries), [](Tie a, Tie b){return Q::comp(a.first, b.first);});
+            for(Tie query : queries){
+                query.second(Q::query_function(query.first));
+            }
+        }
+    };
+}
+
+namespace mytl{
+
+struct Point{
+    ll x, y;
+
+    Point(ll x, ll y) : x{x}, y{y} {};
+    Point(pair<ll,ll> initPair) : x{initPair.first}, y{initPair.second} {};
+
+    pair<ll,ll> getPair(){//not introducing type cast operator for safety
+        return {x,y};
+    }
+
+    //Unary operators
+    Point operator-() const{
+        return {-x, -y};
+    }
+    Point operator+() const{
+        return {x,y};
+    }
+
+    static ll sgn(ll x){
+        return (x > 0) - (x < 0);
+    }
+
+    //Binary operators on ordinary numbers
+    Point operator*(const ll& operand) const{
+        return {x*operand, y*operand};
+    }
+
+    //Binary operators on Point itself
+    Point operator+(const Point& operand) const{
+        return {x+operand.x, y+operand.y};
+    }
+    Point operator-(const Point& operand) const{
+        return {x-operand.x, y-operand.y};
+    }
+
+    ll operator*(const Point& operand){ //vectorial product
+        return x*operand.y - y*operand.x;
+    }
+
+    ll direction(const Point& a, const Point& b) const{
+        return sgn((a - *this) * (b - *this));
+    };
+};
+
+typedef vector<Point> Poly;
+
+bool inside(const Poly& poly, const Point& point, bool strict){
+    set<ll> dirs;
+    for(ll i=0; i<poly.size(); i++){
+        ll nexdindex = (i+1)%poly.size();
+        ll dir = poly[i].direction(poly[nexdindex], point);
+        if(dir == 0){
+            if(strict) return false;
+        }
+        else{
+            dirs.insert(dir);
+            if(dirs.size() > 1) return false;
+        }
+
+    }
+    return true;
+}
+
+}
+namespace mytl{
+template<class Op>
+struct Node : Op::Range{
+    using Range = typename Op::Range;
+    using T = typename Op::T;
+    using Change = typename Op::Change;
+    using Range::singleton;
+    using Range::inside;
+    using Range::intersect;
+    using Range::leftHalf;
+    using Range::rightHalf;
+
+    Node *left_child=NULL, *right_child=NULL;
+
+    T partial;
+    Change pending = Op::identity();
+
+    Node(Range range) : Range(range), partial{Op::initial(range)} {};
+
+    T query(Range range, Change change){
+        return query_(range, change).first;
+    }
+
+    void add(Change change){
+        pending = Op::push(pending, change);
+    }
+
+    void prepare(){
+        if(!singleton()){
+            if(left_child == NULL) left_child = (new Node(leftHalf()));
+            if(right_child == NULL) right_child = (new Node(rightHalf()));
+            left_child->add(pending);
+            right_child->add(pending);
+        }
+        partial = Op::apply(*this, partial, pending);
+        pending = Op::identity();
+    }
+
+    pair<T,T> query_(Range range, Change change){
+        //cout<<range<<" "<<*this<<endl;
+        prepare();
+        if(inside(range)){
+            add(change);
+            prepare();
+            return {partial, Op::zero()};
+        }
+        if(!intersect(range)){
+            //cout<<"n"<<range<<" "<<*this<<endl;
+            return {Op::zero(), partial};
+        }
+
+        auto from_left = left_child->query_(range, change);
+        auto from_right = right_child->query_(range, change);
+        //cout<<from_right.first<<endl;
+        auto needed = Op::reduce(from_left.first, from_right.first), rest = Op::reduce(from_left.second, from_right.second);
+        partial = Op::reduce(needed, rest);
+        return {needed, rest};
+    }
+
+    T build_from(const function<T(ll)>& getter){
+        prepare();
+        pending = Op::identity();
+        if(singleton()) partial = getter(this->first);
+        else partial = Op::reduce(left_child->build_from(getter), right_child->build_from(getter));
+        return partial;
+    }
+
+};
+
+struct Range1D : PairOf<ll>{
+    using PairOf<ll>::pair;
+
+    ll span() const {
+        return second-first+1;
+    }
+    bool singleton() const {
+        return span() == 1;
+    }
+    Range1D leftHalf() const {
+        return Range1D(first, (first+second)/2);
+    }
+    Range1D rightHalf() const {
+        return Range1D((first + second)/2 + 1, second);
+    }
+    bool inside(const Range1D& other) const {
+        return other.first <= first && second <= other.second;
+    }
+    bool intersect(const Range1D& other) const {
+
+        return other.inside(*this)
+             ||(other.first <= first && first <= other.second)
+             ||(other.first <= second && second <= other.second);
+    }
+};
+
+
+}
+
+namespace mytl{
+template<typename T_>
+struct Add_Sum{
+    using Range = Range1D;
+    using T = T_;
+    using Change = T_;
+
+    static Change identity(){
+        return 0;
+    }
+
+    static T zero(){
+        return 0;
+    }
+
+    static T initial(Range r){
+        return zero();
+    }
+
+    static T reduce(T a, T b){
+        return a + b;
+    }
+
+    static T apply(Range r, T a, Change c){
+        return a + r.span()*c;
+    }
+
+    static Change push(Change a, Change b){
+        return a + b;
+    }
+};
+
+template<typename T_>
+struct Multiply_Sum{
+    using Range = Range1D;
+    using T = T_;
+    using Change = T_;
+
+    static Change identity(){
+        return 1;
+    }
+
+    static T zero(){
+        return 0;
+    }
+
+    static T initial(Range r){
+        return r.span()*1;
+    }
+
+    static T reduce(T a, T b){
+        return a + b;
+    }
+
+    static T apply(Range r, T a, Change c){
+        return a*c;
+    }
+
+    static Change push(Change a, Change b){
+        return a*b;
+    }
+};
+
+
+}
+
+namespace mytl{
+/*
+struct Custom_Op{
+    using Range = _;
+    using T = _;
+    using Change = _;
+
+    static Change identity(){
+        return _;
+    }
+
+    static T zero(){
+        return _;
+    }
+
+    static T initial(Range r){
+        return _;
+    }
+
+    static T reduce(T a, T b){
+        return _;
+    }
+
+    static T apply(Range r, T a, Change c){
+        return _;
+    }
+
+    static Change push(Change a, Change b){
+        return _;
+    }
+
+};
+*/
+
+}
+//ENDCOPY
+
+ll d, n, m;
+
+
+MAIN main(){
+    ios_base::sync_with_stdio(false);
+    cin>>n>>d>>m;
+    vector<mytl::Point> kert{mytl::Point(0,d), mytl::Point(d, 0), mytl::Point(n,n-d), mytl::Point(n-d,n)};
+
+    for(ll i=1; i<=m; i++){
+        ll bex, bey;
+        cin>>bex>>bey;
+        mytl::Point akt(bex, bey);
+        if(mytl::inside(kert, akt, false)) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
