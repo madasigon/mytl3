a63927755ef5d31811a58bffa58bb5a1ea241b32
Kiss Gergely <gergely.kiss@lynxanalytics.com>
2018-09-29 18:54:25 +0200

    update extract

--- extracted.cpp
+++ extracted.cpp
@@ -93,112 +93,6 @@ namespace mytl{
 
 }
 namespace mytl{
-template<typename T>
-struct Tracker : optional<T>{
-    using optional<T>::operator=;
-    function<T(T,T)> f;
-
-    Tracker(function<T(T,T)> f) : f{f} {};
-
-    void update(T val){
-        if(this->has_value()){
-            *this = f(this->value(), val);
-        }
-        else{
-            *this = val;
-        }
-    }
-};
-
-template<typename T, typename Container>
-vector<PairOf<T&> > adjecent_pairs(Container& c){
-    vector<PairOf<T&> > res;
-    optional<T*> prev_elem;
-    for(auto& elem : c){
-        if(prev_elem.has_value()){
-            res.push_back({*prev_elem.value(), elem});
-        }
-        prev_elem = &elem;
-    }
-    return res;
-}
-
-template<typename T>
-struct LazyVector : vector<T>{
-    T& operator[](counter_type i){
-        if(i >= this->size()) this->resize(i+1);
-        return vector<T>::operator[](i);
-    }
-};
-
-
-}
-namespace mytl{
-
-template <template<typename, typename...> typename Q, typename R, typename... Args>
-function<R (Args...)> memoize(R (*fn)(Args...)) {
-    Q<R, Args...> table = Q<R, Args...>();
-    return [fn, table](Args... args) mutable -> R {
-        auto res_ = table.get(args...);
-        if(res_.has_value()){
-            return res_.value();
-        }
-        else{
-            auto res = fn(args...);
-            table.set(args..., res);
-            return res;
-        }
-    };
-}
-namespace memo{
-template<typename R, typename Arg>
-struct UnorderedMap : unordered_map<Arg, R>{
-    optional<R> get(Arg arg){
-        auto found = this->find(arg);
-        if(found == this->end()){
-            return {};
-        }
-        else{
-            return (*this)[arg];
-        }
-    }
-
-    void set(Arg arg, R r){
-        (*this)[arg] = r;
-    }
-};
-
-template<typename R, typename Arg>
-struct Vector : mytl::LazyVector<optional<R> >{
-    optional<R> get(Arg arg){
-        return (*this)[arg];
-    }
-    void set(Arg arg, R res){
-        (*this)[arg] = res;
-    }
-};
-
-template<typename R, typename... Args>
-struct Map : map<tuple<Args...>, R> {
-    optional<R> get(Args... args){
-        auto argt = make_tuple(args...);
-        auto found = this->find(argt);
-        if(found == this->end()){
-            return {};
-        }
-        else{
-            return {found->second};
-        }
-    }
-
-    void set(Args... args, R r){
-        (*this)[make_tuple(args...)] = r;
-    }
-};
-}
-
-}
-namespace mytl{
 
 }
 namespace mytl{
@@ -368,6 +262,47 @@ template<class Q>
     };
 }
 
+namespace mytl{
+template<typename T>
+struct Tracker : optional<T>{
+    using optional<T>::operator=;
+    function<T(T,T)> f;
+
+    Tracker(function<T(T,T)> f) : f{f} {};
+
+    void update(T val){
+        if(this->has_value()){
+            *this = f(this->value(), val);
+        }
+        else{
+            *this = val;
+        }
+    }
+};
+
+template<typename T, typename Container>
+vector<PairOf<T&> > adjecent_pairs(Container& c){
+    vector<PairOf<T&> > res;
+    optional<T*> prev_elem;
+    for(auto& elem : c){
+        if(prev_elem.has_value()){
+            res.push_back({*prev_elem.value(), elem});
+        }
+        prev_elem = &elem;
+    }
+    return res;
+}
+
+template<typename T>
+struct LazyVector : vector<T>{
+    T& operator[](counter_type i){
+        if(i >= this->size()) this->resize(i+1);
+        return vector<T>::operator[](i);
+    }
+};
+
+
+}
 namespace mytl{
 
 struct Point{
@@ -631,6 +566,71 @@ struct Custom_Op{
 
 };
 */
+
+}
+namespace mytl{
+
+template <template<typename, typename...> typename Q, typename R, typename... Args>
+function<R (Args...)> memoize(R (*fn)(Args...)) {
+    Q<R, Args...> table = Q<R, Args...>();
+    return [fn, table](Args... args) mutable -> R {
+        auto res_ = table.get(args...);
+        if(res_.has_value()){
+            return res_.value();
+        }
+        else{
+            auto res = fn(args...);
+            table.set(args..., res);
+            return res;
+        }
+    };
+}
+namespace memo{
+template<typename R, typename Arg>
+struct UnorderedMap : unordered_map<Arg, R>{
+    optional<R> get(Arg arg){
+        auto found = this->find(arg);
+        if(found == this->end()){
+            return {};
+        }
+        else{
+            return (*this)[arg];
+        }
+    }
+
+    void set(Arg arg, R r){
+        (*this)[arg] = r;
+    }
+};
+
+template<typename R, typename Arg>
+struct Vector : mytl::LazyVector<optional<R> >{
+    optional<R> get(Arg arg){
+        return (*this)[arg];
+    }
+    void set(Arg arg, R res){
+        (*this)[arg] = res;
+    }
+};
+
+template<typename R, typename... Args>
+struct Map : map<tuple<Args...>, R> {
+    optional<R> get(Args... args){
+        auto argt = make_tuple(args...);
+        auto found = this->find(argt);
+        if(found == this->end()){
+            return {};
+        }
+        else{
+            return {found->second};
+        }
+    }
+
+    void set(Args... args, R r){
+        (*this)[make_tuple(args...)] = r;
+    }
+};
+}
 
 }
 //ENDCOPY
