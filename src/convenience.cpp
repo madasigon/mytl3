7905be9e47957c11c95b8373639fef05c2df20a6
Kiss Gergely <gergely.kiss@lynxanalytics.com>
2018-10-04 20:24:10 +0200

    add some cout operators

--- src/convenience.cpp
+++ src/convenience.cpp
@@ -67,6 +67,52 @@ istream& operator>>(istream& os, Void x){
     return os;
 }
 
+template<typename P, typename Q>
+istream& operator>>(istream& is, pair<P,Q>& x){
+    return is>>x.first>>x.second;
+}
+
+template<typename P, typename Q>
+ostream& operator<<(ostream& os, const pair<P,Q>& x){
+    return os<<"("<<x.first<<", "<<x.second<<")";
+}
+
+template<typename T>
+ostream& print_elements(const T& t, string open="", string separator=" ", string close="", ostream& os=cout){
+    os<<open;
+    bool first = true;
+    for(const auto& elem : t){
+        if(!first) os<<separator;
+        os<<elem;
+        first = false;
+    }
+    os<<close;
+    return os;
+}
+
+
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T>& t){
+    return print_elements(t, "[", ", ", "]", os);
+}
+template<typename T>
+ostream& operator<<(ostream& os, const set<T>& t){
+    return print_elements(t, "{", ", ", "}", os);
+}
+template<typename T>
+ostream& operator<<(ostream& os, const unordered_set<T>& t){
+    return print_elements(t, "{", ", ", "}", os);
+}
+template<typename P, typename Q>
+ostream& operator<<(ostream& os, const map<P, Q>& t){
+    return print_elements(t, "{", ", ", "}", os);
+}
+template<typename P, typename Q>
+ostream& operator<<(ostream& os, const unordered_map<P, Q>& t){
+    return print_elements(t, "{", ", ", "}", os);
+}
+
+
 template<typename T>
 struct Lazy : optional<T>{
 
@@ -74,7 +120,7 @@ struct Lazy : optional<T>{
 
     Lazy(function<T()> f) : f{f}, optional<T>() {};
 
-    T value(){
+    inline T& value(){
         if(!this->has_value()){
             optional<T>::operator=(f());
         }
@@ -84,7 +130,7 @@ struct Lazy : optional<T>{
 };
 
 #define LAZY(val, tipe) mytl::Lazy<tipe>([&](){return (val);})
-
+#define WATCH(x) cout << (#x) << " is " << (x) << endl
 }
 //ENDCOPY
 
