61d1338c350d14e8d46a0b886fba9d53cfc2d610
Kiss Gergely <gergely.kiss@lynxanalytics.com>
2018-10-04 13:46:12 +0200

    extract

--- extracted.cpp
+++ extracted.cpp
@@ -735,67 +735,20 @@ struct Custom_Op{
 }
 namespace mytl{
 
-template <template<typename, typename...> typename Q, typename R, typename... Args>
-function<R (Args...)> memoize(R (*fn)(Args...)) {
-    Q<R, Args...> table = Q<R, Args...>();
-    return [fn, table](Args... args) mutable -> R {
-        auto res_ = table.get(args...);
-        if(res_.has_value()){
-            return res_.value();
+template <template<typename, typename> typename Q, typename Arg, typename R>
+function<R (Arg)> memoize(R (*fn)(Arg)) {
+    Q<Arg, optional<R> > table;
+    return [fn, table](Arg arg) mutable -> R {
+        optional<R>& res = table[arg];
+        if(!res.has_value()){
+            res = fn(arg);
         }
-        else{
-            auto res = fn(args...);
-            table.set(args..., res);
-            return res;
-        }
+        return res.value();
     };
 }
-namespace memo{
-template<typename R, typename Arg>
-struct UnorderedMap : unordered_map<Arg, R>{
-    optional<R> get(Arg arg){
-        auto found = this->find(arg);
-        if(found == this->end()){
-            return {};
-        }
-        else{
-            return (*this)[arg];
-        }
-    }
 
-    void set(Arg arg, R r){
-        (*this)[arg] = r;
-    }
-};
-
-template<typename R, typename Arg>
-struct Vector : mytl::LazyVector<optional<R> >{
-    optional<R> get(Arg arg){
-        return (*this)[arg];
-    }
-    void set(Arg arg, R res){
-        (*this)[arg] = res;
-    }
-};
-
-template<typename R, typename... Args>
-struct Map : map<tuple<Args...>, R> {
-    optional<R> get(Args... args){
-        auto argt = make_tuple(args...);
-        auto found = this->find(argt);
-        if(found == this->end()){
-            return {};
-        }
-        else{
-            return {found->second};
-        }
-    }
-
-    void set(Args... args, R r){
-        (*this)[make_tuple(args...)] = r;
-    }
-};
-}
+template<typename Arg, typename R>
+using MemoVector = LazyVector<R>;
 
 }
 //ENDCOPY
