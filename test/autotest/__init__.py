b0790bf84d25a4c49b589a23b43213e0ffb0939d
Kiss Gergely <gergely.kiss@lynxanalytics.com>
2018-10-07 13:48:17 +0200

    make python script create codeforces_temp directory

--- test/autotest/__init__.py
+++ test/autotest/__init__.py
@@ -5,6 +5,8 @@ import os
 
 def sync(library_path, src_path, dst_path):
     library = collect_dir(library_path)
+    if not os.path.exists(dst_path):
+        os.makedirs(dst_path)
     for filename in filename_list(src_path):
         transform(populate(library), os.path.join(src_path, filename), os.path.join(dst_path, filename))
 
