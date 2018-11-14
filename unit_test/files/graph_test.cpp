b85ab3d845e552ce999d5cb17af6db062958c11d
Kiss Gergely <gergely.kiss@lynxanalytics.com>
2018-11-14 11:39:54 +0100

    queue graph algorithm interfavce change

--- unit_test/files/graph_test.cpp
+++ unit_test/files/graph_test.cpp
@@ -16,11 +16,11 @@ void graph_test(){
     g.newEdge(4,2,2);
 
 
-    using Dijkstra = mytl::AlgoComposer<mytl::Priority, mytl::JustLength>;
-    using BFS = mytl::AlgoComposer<mytl::FIFO, mytl::SimpleJustLength>;
+    //using Dijkstra = mytl::AlgoComposer<mytl::Priority, mytl::JustLength>;
+    //using BFS = mytl::AlgoComposer<mytl::FIFO, mytl::SimpleJustLength>;
     
-    auto x = mytl::queue_graph_algorithm<G,Dijkstra::A>(g, vector<pair<double,ll> >{{0,1}});
-    auto y = mytl::queue_graph_algorithm<G,BFS::A>(g, {{0,1}});
+    auto x = mytl::queue_graph_algorithm<G, mytl::Priority, mytl::JustLength>(g, vector<pair<double,ll> >{{0,1}});
+    auto y = mytl::queue_graph_algorithm<G,mytl::FIFO, mytl::SimpleJustLength>(g, vector<pair<ll,ll> >{{0,1}});
 
     vector<pair<double,ll> > res;
     for(ll i=1; i<=g.n.value(); i++){
