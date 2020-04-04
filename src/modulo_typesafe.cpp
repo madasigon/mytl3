d9081c7db63eca4b6f0917584928ef22c59b3535
Gergely <you@example.com>
2020-04-04 13:24:31 +0200

    remove dangerous TSModulo cout operator

--- src/modulo_typesafe.cpp
+++ src/modulo_typesafe.cpp
@@ -106,12 +106,6 @@ namespace mytl{
 
     using Mod107 = TSModulo<1000000007LL>;
 
-    template<ll MOD>
-    ostream& operator<<(ostream& os, TSModulo<MOD> x){
-        return os<<"("<<x.get()<<"%"<<MOD<<")";
-    }
-
-
 }
 //ENDCOPY
 
--- unit_test/files/modulo_typesafe_test.cpp
+++ unit_test/files/modulo_typesafe_test.cpp
@@ -7,7 +7,6 @@ using namespace std;
 
 void modulo_typesafe_test(){
     mytl::Mod107 x = 5;
-    cout<<x/5;
 
 	mytl::optional<mytl::Mod107> v;
 }
