8da3ca51ac677797e3a32ece5f2780e379eb694f
Kiss Gergely <itisgergo@gmail.com>
2020-04-19 23:15:34 +0000

    unbloat io

--- src/io.cpp
+++ src/io.cpp
@@ -6,73 +6,46 @@
 
 //STARTCOPY
 
-namespace std{
-
-
-istream& operator>>(istream& is, Void& x){
-    return is;
-}
-
-template<typename P, typename Q>
-istream& operator>>(istream& is, pair<P,Q> x){
-    return is>>x.first>>x.second;
-}
-
-
-template<typename P, typename Q>
-ostream& operator<<(ostream& os, const pair<P,Q>& x){
-    os<<"("<<x.first<<", "<<x.second<<")";
-    return os;
-}
-
-}
-
 namespace mytl{
 
-template<typename T, typename P=T>
+template<typename T>
 T read(istream& is=cin){
-    P a;
+    T a;
     is>>a;
-    return T(a);
+    return a;
 }
 
-template<typename T, typename P, typename Q>
-T read(istream& is=cin){
-    P a;
-    Q b;
-    is>>a>>b;
-    return T(a,b);
-}
-
-template<typename T, typename P, typename Q, typename R>
-T read(istream& is=cin){
-    P a;
-    Q b;
-    R c;
-    is>>a>>b>>c;
-    return T(a,b,c);
-}
-
-
-template<typename T, typename... Q, typename Container=vector<T>>
+template<typename T, typename Container=vector<T>>
 vector<T> readValues(ll n, istream& is=cin){
     vector<T> res;
     repeat(n, [&res](){
-        res.push_back(read<T,Q...>());
+        res.push_back(read<T>());
     });
     return Container(res.begin(), res.end());
 }
-
-}
-
-
-namespace mytl{
 
 
 template<typename T>
 void print(const T& x, ostream& os=cout){
     os<<x;
 }
+
+template<typename Container>
+void printContainer(const Container& cont, ostream& os=cout){
+    os<<"{";
+    bool first = true;
+    for(const auto& elem : cont){
+        if(!first){
+            os<<", ";
+        }
+        else{
+            first = false;
+        }
+        print(elem, os);
+    }
+    os<<"}";
+}
+
 
 }
 //ENDCOPY
