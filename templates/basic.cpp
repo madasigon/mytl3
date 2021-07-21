3b8e744246937d16dcf07dcb8ba7aa4647ecff09
Gergely Kiss <placeholder@example.com>
2021-07-21 17:44:13 +0200

    extract

--- templates/basic.cpp
+++ templates/basic.cpp
@@ -39,220 +39,6 @@ using Void = tuple<>;
 #endif
 
 namespace mytl{
-template<typename T>
-T power(T base, ll exponential, T unit=1){
-    T res = unit;
-    while(exponential > 0){
-        if(exponential%2 == 1) res = res * base;
-        base = base * base;
-        exponential = exponential / 2;
-    }
-    return res;
-}
-
-template<typename T>
-struct Pows2 : vector<T>{
-    Pows2(){
-        this->resize(300);
-        this->operator[](0) = 1;
-        for(ll i=1; i < this->size(); i++){
-            this->operator[](i) = this->operator[](i) * 2;
-        }
-    }
-};
-
-template<typename T>
-Pows2<T> powers2;
-
-}
-namespace mytl{
-
-template<typename T>
-struct LazyVector : vector<T> {
-	inline typename vector<T>::reference operator[](need_int i) {
-
-		if (i >= vector<T>::size()) vector<T>::resize(i + 1);
-		return vector<T>::operator[](i);
-	}
-};
-
-template<typename T>
-struct TwoWayLazyVector {
-	LazyVector<T> negative, positive;
-	inline typename vector<T>::reference operator[](need_int i) {
-		if(i >= 0){
-			return positive[i];
-		}
-		else {
-			return negative[-i];
-		}
-	}
-};
-
-
-template<typename T>
-struct optional {
-	T *ptr = nullptr;
-
-	inline void set(const T& val) {
-		ptr = new T(val);
-	}
-
-	inline optional() {}
-	inline optional(const T& val) {
-		set(val);
-	}
-
-	optional& operator=(const T& val){
-		set(val);
-		return *this;
-	}
-
-	~optional() {
-		delete ptr;
-	}
-	inline T value() const {
-		return *ptr;
-	}
-	inline bool has_value() const {
-		return ptr != nullptr;
-	}
-
-	
-
-	optional& operator=(const optional& other) {
-		if (other.has_value()) {
-			set(other.value());
-		}
-		else {
-			ptr = nullptr;
-		}
-		return *this;
-	}
-	optional(const optional& other) {
-		operator=(other);
-	}
-
-	optional(optional&& other) {
-		ptr = other.ptr;
-		other.ptr = nullptr;
-	}
-
-	optional& operator=(optional&& other) {
-		if (this != &other) {
-			delete ptr;
-			ptr = other.ptr;
-			other.ptr = nullptr;
-			return *this;
-		}
-	}
-
-};
-
-}
-namespace mytl{
-    template<ll MOD>
-    struct TSModulo{
-		friend class optional<TSModulo<MOD> >;
-    private:
-        ll val;
-
-    public:
-        static TSModulo inverse(TSModulo x){// assuming MOD is prime and x != 0
-            return power(x, MOD-2);
-        };
-		TSModulo(ll initVal) {
-			if (-MOD < initVal && initVal < MOD) {
-				val = initVal;
-			}
-			else {
-				val = initVal % MOD;
-			}
-		}
-        TSModulo() : TSModulo(0) {}
-        TSModulo& operator=(const TSModulo&) = default;
-
-        ll get() const{ // no type cast operator to prevent accidentally turning into ordinary number
-			if (val >= 0) return val;
-			else return val + MOD;
-        }
-
-        //Unary operators
-        TSModulo operator-() const{
-            return TSModulo(-val);
-        };
-        TSModulo operator+() const{
-            return TSModulo(+val);
-        };
-
-        //Binary operators on ordinary numbers
-        TSModulo operator-(const ll& operand) const{
-            return TSModulo(val-operand);
-        };
-        TSModulo operator+(const ll& operand) const{
-            return TSModulo(val+operand);
-        };
-        TSModulo operator*(const ll& operand) const{
-            return TSModulo(val*operand);
-        };
-        TSModulo operator/(const ll& operand) const{ //debug_asserting MOD is prime
-            return TSModulo(inverse(operand) * val);
-        };
-
-        //Binary operators on Modulo
-        TSModulo operator-(const TSModulo& operand) const{
-            return TSModulo(val-operand.get());
-        };
-        TSModulo operator+(const TSModulo& operand) const{
-            return TSModulo(val+operand.get());
-        };
-        TSModulo operator*(const TSModulo& operand) const{
-            return TSModulo(val*operand.get());
-        };
-        TSModulo operator/(const TSModulo& operand) const{ //debug_asserting MOD is prime
-            return TSModulo(val * inverse(operand));
-        };
-    };
-
-	template<ll MOD>
-	struct optional<TSModulo<MOD> >{
-		ll val = off_value;
-		static const ll off_value = -MOD - 1;
-
-		bool has_value() const {
-			return val != off_value;
-		}
-
-		void set(const TSModulo<MOD>& other) {
-			val = other.val;
-		}
-
-		TSModulo<MOD> value() const {
-			debug_assert((has_value()));
-			if (!has_value()) {
-                TSModulo<MOD>* dummy = nullptr;
-				return *dummy;
-			}
-			return TSModulo<MOD>(val);
-		}
-
-		optional() {
-		}
-
-		optional<TSModulo<MOD> >& operator=(const TSModulo<MOD>& other) {
-			set(other);
-			return *this;
-		}
-		optional(const TSModulo<MOD>& other) {
-			set(other);
-		}
-
-	};
-
-    using Mod107 = TSModulo<1000000007LL>;
-
-}
-namespace mytl{
 
 template<typename T, typename R, typename F=function<R(T)> >
 vector<R> fmap(F f, const vector<T>& t){
@@ -277,25 +63,126 @@ void repeat(need_int n, const F& callback){
 
 #define WATCH(x) cout << (#x) << " is " << (x) << endl
 }
+
 namespace mytl{
 
-template<typename T, T(*f)(T,T)>
-struct Tracker : optional<T>{
+template<typename T>
+T read(istream& is=cin){
+    T a;
+    is>>a;
+    return a;
+}
 
-	void update(const T& x){
-		if (optional<T>::has_value()) {
-			optional<T>::set(f(optional<T>::value(), x));
-		}
-		else {
-			optional<T>::set(x);
-		}
+template<typename T, typename Container=vector<T>>
+vector<T> readValues(ll n, istream& is=cin){
+    vector<T> res;
+    repeat(n, [&res](){
+        res.push_back(read<T>());
+    });
+    return Container(res.begin(), res.end());
+}
+
+
+template<typename T>
+void print(const T& x, ostream& os=cout){
+    os<<x;
+}
+
+template<typename Container>
+void printContainer(const Container& cont, ostream& os=cout){
+    os<<"{";
+    bool first = true;
+    for(const auto& elem : cont){
+        if(!first){
+            os<<", ";
+        }
+        else{
+            first = false;
+        }
+        print(elem, os);
     }
+    os<<"}";
+}
+
+
+}
+namespace mytl{
+
+
+struct Point{
+    ll x, y;
+
+    Point(ll x, ll y) : x{x}, y{y} {};
+    Point(pair<ll,ll> initPair) : x{initPair.first}, y{initPair.second} {};
+
+    pair<ll,ll> getPair(){//not introducing type cast operator for safety
+        return {x,y};
+    }
+
+    //Unary operators
+    Point operator-() const{
+        return {-x, -y};
+    }
+    Point operator+() const{
+        return {x,y};
+    }
+
+    static ll sgn(ll x){
+        return (x > 0) - (x < 0);
+    }
+
+    //Binary operators on ordinary numbers
+    Point operator*(const ll& operand) const{
+        return {x*operand, y*operand};
+    }
+
+    //Binary operators on Point itself
+    Point operator+(const Point& operand) const{
+        return {x+operand.x, y+operand.y};
+    }
+    Point operator-(const Point& operand) const{
+        return {x-operand.x, y-operand.y};
+    }
+
+    ll operator*(const Point& operand){ //vectorial product
+        return x*operand.y - y*operand.x;
+    }
+
+    ll direction(const Point& a, const Point& b) const{
+        return sgn((a - *this) * (b - *this));
+    };
 };
 
-template<typename T>
-T min(T a, T b){return std::min(a,b);}
-template<typename T>
-T max(T a, T b){return std::max(a,b);}
+ll distance_squared(const Point& a, const Point& b){
+    return (a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y);
+}
+
+double distance(const Point& a, const Point& b){
+    return sqrt(distance_squared(a, b));
+}
+
+ll cartesian_distance(const Point& a, const Point& b){
+    return abs(a.x - b.x) + abs(a.y - b.y);
+}
+
+typedef vector<Point> Poly;
+
+bool inside(const Poly& poly, const Point& point, bool strict){ //assuming Poly is weakly convex
+    ll prevdir = -2;
+    for(ll i=0; i<poly.size(); i++){
+        ll nexdindex = (i+1)%poly.size();
+        ll dir = poly[i].direction(poly[nexdindex], point);
+        if(dir == 0){
+            if(strict) return false;
+        }
+        else{
+            if(dir != prevdir && prevdir != -2) return false;
+            prevdir = dir;
+        }
+
+    }
+    return true;
+}
 
 }
 namespace mytl{
@@ -580,42 +467,6 @@ return _;
 };
 */
 }
-namespace mytl {
-
-template <typename C, typename Arg, typename R>
-function<R(Arg)> __memoize(R(*fn)(Arg)) {
-	C table;
-	return [fn, table](Arg arg) mutable -> R {
-		if (!table[arg].has_value()) {
-			table[arg].set(fn(arg));
-		}
-		return table[arg].value();
-	};
-}
-
-template<template<typename, typename, typename...> typename C, typename Arg, typename R>
-function<R(Arg)> memoize(R(*fn)(Arg)) {
-	return __memoize<C<Arg, optional<R> >, Arg, R>(fn);
-}
-
-template<typename R>
-function<R (ll) > quick_memoize(R(*fn)(ll)) {
-	return __memoize<TwoWayLazyVector<optional<R> >, ll, R>(fn);
-}
-
-template<typename R>
-function<R(ll, ll)> quick_memoize(R(*fn)(ll, ll)) {
-	TwoWayLazyVector< TwoWayLazyVector<optional<R> > > table;
-	return [fn, table](ll p1, ll p2) mutable -> R {
-		if(!table[p1][p2].has_value()){
-			table[p1][p2].set(fn(p1, p2));
-		}
-		return table[p1][p2].value();
-	};
-}
-
-
-}
 namespace mytl{
 
 template<typename Op_>
@@ -804,147 +655,30 @@ struct Corasick {
 
 }
 namespace mytl{
-
-template<typename T, typename Pred>
-T discrete_binary_search(T l, T r, Pred f){
-	debug_assert(f(l));
-    while(l < r){
-        T pivot = (l+r+1)/2;
-        if(!f(pivot)) r = pivot-1;
-		else l = pivot;
+template<typename T>
+T power(T base, ll exponential, T unit=1){
+    T res = unit;
+    while(exponential > 0){
+        if(exponential%2 == 1) res = res * base;
+        base = base * base;
+        exponential = exponential / 2;
     }
-    return l;
+    return res;
 }
-
-template<typename T, typename Pred>
-T continuous_binary_search(T l, T r, need_int iterations, Pred f){
-    mytl::repeat(iterations,[&](){
-        T pivot = (l+r)/2;
-        if(f(pivot)) l = pivot;
-        else r = pivot;
-    });
-    return l;
-}
-}
-
-namespace mytl{
 
 template<typename T>
-T read(istream& is=cin){
-    T a;
-    is>>a;
-    return a;
-}
-
-template<typename T, typename Container=vector<T>>
-vector<T> readValues(ll n, istream& is=cin){
-    vector<T> res;
-    repeat(n, [&res](){
-        res.push_back(read<T>());
-    });
-    return Container(res.begin(), res.end());
-}
-
-
-template<typename T>
-void print(const T& x, ostream& os=cout){
-    os<<x;
-}
-
-template<typename Container>
-void printContainer(const Container& cont, ostream& os=cout){
-    os<<"{";
-    bool first = true;
-    for(const auto& elem : cont){
-        if(!first){
-            os<<", ";
+struct Pows2 : vector<T>{
+    Pows2(){
+        this->resize(300);
+        this->operator[](0) = 1;
+        for(ll i=1; i < this->size(); i++){
+            this->operator[](i) = this->operator[](i) * 2;
         }
-        else{
-            first = false;
-        }
-        print(elem, os);
     }
-    os<<"}";
-}
-
-
-}
-namespace mytl{
-
-struct Point{
-    ll x, y;
-
-    Point(ll x, ll y) : x{x}, y{y} {};
-    Point(pair<ll,ll> initPair) : x{initPair.first}, y{initPair.second} {};
-
-    pair<ll,ll> getPair(){//not introducing type cast operator for safety
-        return {x,y};
-    }
-
-    //Unary operators
-    Point operator-() const{
-        return {-x, -y};
-    }
-    Point operator+() const{
-        return {x,y};
-    }
-
-    static ll sgn(ll x){
-        return (x > 0) - (x < 0);
-    }
-
-    //Binary operators on ordinary numbers
-    Point operator*(const ll& operand) const{
-        return {x*operand, y*operand};
-    }
-
-    //Binary operators on Point itself
-    Point operator+(const Point& operand) const{
-        return {x+operand.x, y+operand.y};
-    }
-    Point operator-(const Point& operand) const{
-        return {x-operand.x, y-operand.y};
-    }
-
-    ll operator*(const Point& operand){ //vectorial product
-        return x*operand.y - y*operand.x;
-    }
-
-    ll direction(const Point& a, const Point& b) const{
-        return sgn((a - *this) * (b - *this));
-    };
 };
 
-ll distance_squared(const Point& a, const Point& b){
-    return (a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y);
-}
-
-double distance(const Point& a, const Point& b){
-    return sqrt(distance_squared(a, b));
-}
-
-ll cartesian_distance(const Point& a, const Point& b){
-    return abs(a.x - b.x) + abs(a.y - b.y);
-}
-
-typedef vector<Point> Poly;
-
-bool inside(const Poly& poly, const Point& point, bool strict){ //assuming Poly is weakly convex
-    ll prevdir = -2;
-    for(ll i=0; i<poly.size(); i++){
-        ll nexdindex = (i+1)%poly.size();
-        ll dir = poly[i].direction(poly[nexdindex], point);
-        if(dir == 0){
-            if(strict) return false;
-        }
-        else{
-            if(dir != prevdir && prevdir != -2) return false;
-            prevdir = dir;
-        }
-
-    }
-    return true;
-}
+template<typename T>
+Pows2<T> powers2;
 
 }
 namespace mytl{
@@ -1017,6 +751,252 @@ public:
 ll Modulo::CURRENT_MOD = 0;
 
 }
+namespace mytl{
+
+template<typename T>
+struct LazyVector : vector<T> {
+	inline typename vector<T>::reference operator[](need_int i) {
+
+		if (i >= vector<T>::size()) vector<T>::resize(i + 1);
+		return vector<T>::operator[](i);
+	}
+};
+
+template<typename T>
+struct TwoWayLazyVector {
+	LazyVector<T> negative, positive;
+	inline typename vector<T>::reference operator[](need_int i) {
+		if(i >= 0){
+			return positive[i];
+		}
+		else {
+			return negative[-i];
+		}
+	}
+};
+
+
+template<typename T>
+struct optional {
+	T *ptr = nullptr;
+
+	inline void set(const T& val) {
+		ptr = new T(val);
+	}
+
+	inline optional() {}
+	inline optional(const T& val) {
+		set(val);
+	}
+
+	optional& operator=(const T& val){
+		set(val);
+		return *this;
+	}
+
+	~optional() {
+		delete ptr;
+	}
+	inline T value() const {
+		return *ptr;
+	}
+	inline bool has_value() const {
+		return ptr != nullptr;
+	}
+
+	
+
+	optional& operator=(const optional& other) {
+		if (other.has_value()) {
+			set(other.value());
+		}
+		else {
+			ptr = nullptr;
+		}
+		return *this;
+	}
+	optional(const optional& other) {
+		operator=(other);
+	}
+
+	optional(optional&& other) {
+		ptr = other.ptr;
+		other.ptr = nullptr;
+	}
+
+	optional& operator=(optional&& other) {
+		if (this != &other) {
+			delete ptr;
+			ptr = other.ptr;
+			other.ptr = nullptr;
+			return *this;
+		}
+	}
+
+};
+
+}
+namespace mytl {
+
+template <typename C, typename Arg, typename R>
+function<R(Arg)> __memoize(R(*fn)(Arg)) {
+	C table;
+	return [fn, table](Arg arg) mutable -> R {
+		if (!table[arg].has_value()) {
+			table[arg].set(fn(arg));
+		}
+		return table[arg].value();
+	};
+}
+
+template<template<typename, typename, typename...> typename C, typename Arg, typename R>
+function<R(Arg)> memoize(R(*fn)(Arg)) {
+	return __memoize<C<Arg, optional<R> >, Arg, R>(fn);
+}
+
+template<typename R>
+function<R (ll) > quick_memoize(R(*fn)(ll)) {
+	return __memoize<TwoWayLazyVector<optional<R> >, ll, R>(fn);
+}
+
+template<typename R>
+function<R(ll, ll)> quick_memoize(R(*fn)(ll, ll)) {
+	TwoWayLazyVector< TwoWayLazyVector<optional<R> > > table;
+	return [fn, table](ll p1, ll p2) mutable -> R {
+		if(!table[p1][p2].has_value()){
+			table[p1][p2].set(fn(p1, p2));
+		}
+		return table[p1][p2].value();
+	};
+}
+
+
+}
+namespace mytl{
+
+template<typename T, typename Pred>
+T discrete_binary_search(T l, T r, Pred f){
+	debug_assert(f(l));
+    while(l < r){
+        T pivot = (l+r+1)/2;
+        if(!f(pivot)) r = pivot-1;
+		else l = pivot;
+    }
+    return l;
+}
+
+template<typename T, typename Pred>
+T continuous_binary_search(T l, T r, need_int iterations, Pred f){
+    mytl::repeat(iterations,[&](){
+        T pivot = (l+r)/2;
+        if(f(pivot)) l = pivot;
+        else r = pivot;
+    });
+    return l;
+}
+}
+namespace mytl{
+    template<ll MOD>
+    struct TSModulo{
+		friend class optional<TSModulo<MOD> >;
+    private:
+        ll val;
+
+    public:
+        static TSModulo inverse(TSModulo x){// assuming MOD is prime and x != 0
+            return power(x, MOD-2);
+        };
+		TSModulo(ll initVal) {
+			if (-MOD < initVal && initVal < MOD) {
+				val = initVal;
+			}
+			else {
+				val = initVal % MOD;
+			}
+		}
+        TSModulo() : TSModulo(0) {}
+        TSModulo& operator=(const TSModulo&) = default;
+
+        ll get() const{ // no type cast operator to prevent accidentally turning into ordinary number
+			if (val >= 0) return val;
+			else return val + MOD;
+        }
+
+        //Unary operators
+        TSModulo operator-() const{
+            return TSModulo(-val);
+        };
+        TSModulo operator+() const{
+            return TSModulo(+val);
+        };
+
+        //Binary operators on ordinary numbers
+        TSModulo operator-(const ll& operand) const{
+            return TSModulo(val-operand);
+        };
+        TSModulo operator+(const ll& operand) const{
+            return TSModulo(val+operand);
+        };
+        TSModulo operator*(const ll& operand) const{
+            return TSModulo(val*operand);
+        };
+        TSModulo operator/(const ll& operand) const{ //debug_asserting MOD is prime
+            return TSModulo(inverse(operand) * val);
+        };
+
+        //Binary operators on Modulo
+        TSModulo operator-(const TSModulo& operand) const{
+            return TSModulo(val-operand.get());
+        };
+        TSModulo operator+(const TSModulo& operand) const{
+            return TSModulo(val+operand.get());
+        };
+        TSModulo operator*(const TSModulo& operand) const{
+            return TSModulo(val*operand.get());
+        };
+        TSModulo operator/(const TSModulo& operand) const{ //debug_asserting MOD is prime
+            return TSModulo(val * inverse(operand));
+        };
+    };
+
+	template<ll MOD>
+	struct optional<TSModulo<MOD> >{
+		ll val = off_value;
+		static const ll off_value = -MOD - 1;
+
+		bool has_value() const {
+			return val != off_value;
+		}
+
+		void set(const TSModulo<MOD>& other) {
+			val = other.val;
+		}
+
+		TSModulo<MOD> value() const {
+			debug_assert((has_value()));
+			if (!has_value()) {
+                TSModulo<MOD>* dummy = nullptr;
+				return *dummy;
+			}
+			return TSModulo<MOD>(val);
+		}
+
+		optional() {
+		}
+
+		optional<TSModulo<MOD> >& operator=(const TSModulo<MOD>& other) {
+			set(other);
+			return *this;
+		}
+		optional(const TSModulo<MOD>& other) {
+			set(other);
+		}
+
+	};
+
+    using Mod107 = TSModulo<1000000007LL>;
+
+}
 namespace mytl {
 
 template<class T>
@@ -1084,6 +1064,27 @@ struct Resetter {
     };
 
 };
+}
+namespace mytl{
+
+template<typename T, T(*f)(T,T)>
+struct Tracker : optional<T>{
+
+	void update(const T& x){
+		if (optional<T>::has_value()) {
+			optional<T>::set(f(optional<T>::value(), x));
+		}
+		else {
+			optional<T>::set(x);
+		}
+    }
+};
+
+template<typename T>
+T min(T a, T b){return std::min(a,b);}
+template<typename T>
+T max(T a, T b){return std::max(a,b);}
+
 }
 //ENDCOPY
 
